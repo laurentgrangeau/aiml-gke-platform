376d64070abef9150d83524911b7f92002b49b97
Laurent Grangeau <laurent.grangeau@gmail.com>
2024-02-19 17:25:02 +0100

    feat: Fixed version and typo

--- versions.tf
+++ versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">=1.0"
+  required_version = ">= 1.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.16.0"
+      version = ">= 5.16.0"
     }
     google-beta = {
-      source  = "hashicorp/google"
-      version = "5.16.0"
+      source  = "hashicorp/google-beta"
+      version = ">= 5.16.0"
     }
   }
 }
